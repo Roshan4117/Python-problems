3b2de612e9c789a7bbe749b6786d093d09fdfc4e
Roshan4117 <115645090+Roshan4117@users.noreply.github.com>
2025-01-18 16:27:05 +0530

    Update Duplicate values.py

--- Duplicate values.py
+++ Duplicate values.py
@@ -23,3 +23,7 @@ def dup(x):
 
 x = [1,1,2,2,2,3,4,5,6,6,6,6]
 print("",dup(x))
+
+#This Python Code shows which elements in the List is repeted multiple times and also shows how many times it is present in the List.
+#First the List is made smaller by removing elements repeated twice and then counts the number of times each elements is repeated.
+#Incase of elements count is same then the biggest number elements is taken into consideration and the result is printed
