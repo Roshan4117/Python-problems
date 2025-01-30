684e75d5488c780f15e9ecc676eefe69b73bfb12
Roshan4117 <115645090+Roshan4117@users.noreply.github.com>
2025-01-31 00:58:52 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -1,4 +1,4 @@
 s1 =input("Enter any string: ")
 for i in range(len(s1)):
-    for j in range(i+1, len(s1)+1):
+    for j in range(i+1, len(s1)+1): # a small program that has a substring and prints the substring by iterating each staring index to ending indices
         print(s1[i:j])
