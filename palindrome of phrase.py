735efb4bb029dfe7d9f75ed13e861e309101e269
Roshan4117 <115645090+Roshan4117@users.noreply.github.com>
2025-01-31 00:50:50 +0530

    Update palindrome of phrase.py

--- palindrome of phrase.py
+++ palindrome of phrase.py
@@ -1,12 +1,12 @@
 s = input("Enter the phrase:")
 result = "" 
 for str in s:
-    if ( "A" <= str <= "Z") or ("a" <= str <= "z"):
+    if ( "A" <= str <= "Z") or ("a" <= str <= "z"):# here the empty spaces, special character are removed because the range is only between alphabets 
         result += str
 print (result)
 
 for i in range( len(result) // 2):
-    if result[i] != result[-(i + 1)]:
+    if result[i] != result[-(i + 1)]:# here when the spaces and special characters are removed it checks if each elements in the first and last are equal or not 
         print(False)
         break
     else:
@@ -14,6 +14,6 @@ for i in range( len(result) // 2):
         break
         
 if True:
-    print("palindrome")
+    print("palindrome")# prints is palindrome or not
 else:
     print("not palindrome")
