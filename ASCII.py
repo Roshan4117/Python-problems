6310720478bf62e146ad66b32ce2850c63979418
Roshan4117 <115645090+Roshan4117@users.noreply.github.com>
2025-01-30 15:18:20 +0530

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -4,17 +4,16 @@ def ascii(s):
     for char in s:
         ascii_value = 0
         for i in range(256):  
-            if chr(i) == char: 
+            if chr(i) == char: # using the built in function chr() I can identify the ascii value of the given alphabet 
                 ascii_value = i
                 break
-        values.append(ascii_value)
+        values.append(ascii_value) # here appending the ascii value to the list values
 
     return values
 
-# Test the function
 input_string = "Hello"
 values = ascii(input_string)
-print(f"ASCII values of '{input_string}': {values}")
+print("ASCII values : ", input_string, values)
 
 
 #The ascii function takes a string as input and returns a list of ASCII values corresponding to each character in the string. It iterates through each character in the string and uses a loop to find the ASCII value by comparing the character to each possible ASCII value (from 0 to 255). Once a match is found, the ASCII value is added to the list 
