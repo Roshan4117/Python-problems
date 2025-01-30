92fb234ac8e042a7b2c95dfc082d32d9f5827e2c
Roshan4117 <115645090+Roshan4117@users.noreply.github.com>
2025-01-31 00:48:45 +0530

    Update div by 27.py

--- div by 27.py
+++ div by 27.py
@@ -1,7 +1,7 @@
 n = [2,54,819,81,100]
 l = len(n)
 count = 0
-for i in range(0, l):
+for i in range(0, l):# checks in te list if each element is divisible by 27 or not and also tells if the number is divisible by 27 or not
     if n[i] % 27 ==0:
         n[i] - 1
         print("divisible by 27")
