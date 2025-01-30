71a54ee072e7b95312075d4b127b27001072881d
Roshan4117 <115645090+Roshan4117@users.noreply.github.com>
2025-01-31 00:46:49 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -5,7 +5,7 @@ def is_palindrome(s):
     return True
 
 string = "radar"
-print(f"Is '{string}' a palindrome? {is_palindrome(string)}")
+print("Is '{string}' a palindrome? {is_palindrome(string)}")
 
 #This Python Code checks if a given string is a palindrome.The Code works by iterating through the first half of the string, comparing each character with its corresponding character from the end of the string by using negative indexing which starts from the back of the string. If any pair of characters do not match then the function immediately returns False. If all pairs match, the function returns True, indicating that the string is a palindrome. 
 #A palindrome is a word, phrase, or sequence that reads the same forwards and backwards, ignoring spaces and punctuation.
