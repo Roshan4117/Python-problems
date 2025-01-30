7d12647f5c20055f390acff185593c50d7df3f09
Roshan4117 <115645090+Roshan4117@users.noreply.github.com>
2025-01-31 00:44:34 +0530

    Update washing machine logic.py

--- washing machine logic.py
+++ washing machine logic.py
@@ -1,18 +1,21 @@
 Weight = int(input(""))
-water_lvl = ("L","M","H")
+water_lvl = input("")
 
-if (0 < Weight <= 2000): # washing machine logic where it accepts weight under 7000g and cant exceed because it will result in Invalid input
-    timer = 25
-    print("",Weight,water_lvl[0])
-elif (2000 < Weight <= 4000):
-    timer = 35
-    print("",Weight,water_lvl[1])
-elif (4000 < Weight <= 7000):
-    timer = 45
-    print("",Weight,water_lvl[2])
+if (0 < Weight <= 2000):# here weight is between 0 to 2000g so it will run on low water within 25 minutes
+    if water_lvl == "L":
+        timer = 25
+    print("timer estimated:",timer)
+elif (2000 < Weight <= 4000):# here weight is between 2001g to 4000g so it will run on medium water within 35 minutes
+    if water_lvl == "M":# 
+        timer = 35
+    print("timer estimated",timer)
+elif (4000 < Weight <= 7000):# here weight is between 4001 to 7000g so it will run on High water within 45 minutes
+    if water_lvl == "H":
+        timer = 45
+    print("timer estimated:",timer)
 elif (Weight > 7000):
-    print("OVERLOAD")
+    print("OVERLOAD")# here weight is over the maximum weight so it will overload and stop here and the rest is belowe 0 which is invalid 
 elif ( Weight <= 0):
     print("Invalid")
 else:
-    print(" 0g 0 minutes")
+    print(" 0g 0 minutes") # if no inpput is given then it wont run at all 
